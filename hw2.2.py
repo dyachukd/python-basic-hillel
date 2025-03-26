d23eb1f402e72339e187aa77cfceb4fff07d901b
dyachukd <dyachuk.ddd@gmail.com>
2025-03-26 17:01:43 +0200

    edited hw2.2

--- hw2.2.py
+++ hw2.2.py
@@ -1,8 +1,8 @@
 user_value = int(input('введіть 5-ти значне ціле число: '))
-first_num = user_value // 10000
-second_num = user_value // 1000 % 10 
-third_num = user_value // 100 % 10 
-fourth_num = user_value // 10 % 10
-fifth_num = user_value % 10
-
-print(fifth_num,fourth_num,third_num,second_num,first_num)
+first_digit= user_value // 10000
+second_digit = user_value // 1000 % 10 
+third_digit = user_value // 100 % 10 
+fourth_digit = user_value // 10 % 10
+fifth_digit = user_value % 10
+reversed_number = fifth_digit * 10000 + fourth_digit * 1000 + third_digit * 100 + second_digit * 10 + first_digit
+print(reversed_number)
