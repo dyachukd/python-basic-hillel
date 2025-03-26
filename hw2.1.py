28962dc90f9df297ff02ff731cc4cd77af387799
dyachukd <dyachuk.ddd@gmail.com>
2025-03-26 16:42:08 +0200

    added hw2.1

---
+++ hw2.1.py
@@ -0,0 +1,9 @@
+user_value = int(input('введіть 4-х значне ціле число'))
+first_num = user_value // 1000
+second_num = user_value // 100 % 10 
+third_num = user_value // 10 % 10 
+fourth_num = user_value % 10
+print(first_num)
+print(second_num)
+print(third_num)
+print(fourth_num)
