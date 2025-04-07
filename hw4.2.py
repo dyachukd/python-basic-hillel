d466cfa4ba352d0c7228983d548afed5f9619c8f
dyachukd <dyachuk.ddd@gmail.com>
2025-04-07 18:17:36 +0300

    edited hw 4.2

--- hw4.2.py
+++ hw4.2.py
@@ -1,6 +1,6 @@
-lst = [0, 1, 7, 2, 4, 8] 
+#lst = [0, 1, 7, 2, 4, 8] 
 #lst = [1, 3, 5] 
-#lst = [6] 
+lst = [6] 
 #lst = [] 
 
 if not lst:
@@ -10,6 +10,7 @@ else:
     i = 0
     while i < len(lst):
             if i % 2 == 0:
-                sum += lst[i]
-            i += 1
-    print( sum * lst[-1])
+                sum = sum + lst[i]
+            i = i + 1  
+    sum = sum * lst[-1]
+    print(sum)
