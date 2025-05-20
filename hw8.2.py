48d130ec4c9a4f2766d0a750bdc0d9b7df245343
dyachukd <dyachuk.ddd@gmail.com>
2025-05-20 14:53:56 +0300

    Update hw8.2.py

--- hw8.2.py
+++ hw8.2.py
@@ -4,7 +4,6 @@ def is_palindrome(text):
     cleaned = ''.join(ch.lower() for ch in text if ch.isalnum())
     return cleaned == cleaned[::-1]
 
-# Тести
 assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1' 
 assert is_palindrome('0P') == False, 'Test2' 
 assert is_palindrome('a.') == True, 'Test3' 
